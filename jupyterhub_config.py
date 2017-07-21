7c33107f083cc4e28a165e0d59c844abe3c5c029
Shane Canon <scanon@lbl.gov>
2017-07-21 15:26:06 -0700

    Cleanup config

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -2,12 +2,14 @@ import os
 c.JupyterHub.hub_ip = '0.0.0.0'
 c.JupyterHub.hub_port = 8082
 
-c.Spawner.cmd = ['/global/common/cori/software/python/3.5-anaconda/bin/jupyterhub-singleuser']
+bindir='/global/common/cori/software/python/3.5-anaconda/bin/
+if 'BASE_PATH' in os.environ:
+    bindir=os.environ['BASE_PATH']+'/bin/' 
+c.Spawner.cmd = [bindir+'jupyterhub-singleuser']
 
 c.Spawner.ip = '0.0.0.0'
 
 c.JupyterHub.authenticator_class = 'gsiauthenticator.auth.GSIAuthenticator'
-#c.Authenticator.admin_users = {'rthomas', 'canon', 'shreyas'}
 if 'ADMINS' in os.environ:
     c.Authenticator.admin_users = os.environ['REMOTE_HOST'].split(',')
 
@@ -34,7 +36,7 @@ if 'HUB_API_URL' in os.environ:
     c.SSHSpawner.hub_api_url=os.environ['HUB_API_URL']
 
 c.SSHSpawner.use_gsi = True
-c.SSHSpawner.path = '/global/common/cori/software/python/3.5-anaconda/bin:/global/common/cori/das/jupyterhub/:/usr/common/usg/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games:/usr/lib/mit/bin:/usr/lib/mit/sbin'
+c.SSHSpawner.path = bindir+':/global/common/cori/das/jupyterhub/:/usr/common/usg/bin:/usr/bin:/bin'
 c.SSHSpawner.remote_port_command = '/global/common/cori/das/jupyterhub/get_port.py'
 
 c.JupyterHub.cookie_max_age_days = 0.5
