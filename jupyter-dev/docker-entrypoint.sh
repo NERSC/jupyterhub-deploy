ccc361cb060366576cf3f0c2b917bc31149061ce
Rollin Thomas <rcthomas@lbl.gov>
2017-10-21 08:43:08 -0700

    Entrypoint script for JupyterHub

---
+++ jupyter-dev/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# file_env VAR [DEFAULT]
+# ----------------------
+# Treat the value of VAR_FILE as the path to a secrets file and initialize VAR
+# with the contents of that file.  From postgres docker-entrypoint.sh.
+
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [ "${!var:-}" ]; then
+        echo 1
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        echo 2
+        val="$(< "${!fileVar}")"
+    fi
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
+file_env 'POSTGRES_PASSWORD'
+
+exec "$@"
