cc510d7dc468bef176e3ad462b416258396737af
Shane Canon <scanon@lbl.gov>
2018-07-02 15:44:50 -0700

    Fix type in entrypoint for sshspawner

--- jupyter-sshspawner/app/docker-entrypoint.sh
+++ jupyter-sshspawner/app/docker-entrypoint.sh
@@ -3,9 +3,9 @@
 for u in $(ls /home/) ; do 
   mkdir /home/$u/.ssh/
   cat /config/newkey.pub > /home/$u/.ssh/authorized_keys
-  chmod 700 /home/$su/.ssh/
+  chmod 700 /home/$u/.ssh/
   chown -R $u /home/$u/.ssh
-  chmod 600 /home/$su/.ssh/authorized_keys
+  chmod 600 /home/$u/.ssh/authorized_keys
 done
 
 exec "$@"
