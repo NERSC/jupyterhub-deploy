6df3731755974cca8e47639158cb62d0f89888b8
Rollin Thomas <rcthomas@lbl.gov>
2018-10-11 13:48:15 -0700

    Build script

--- base/build.sh
+++ base/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build --no-cache -t registry.spin.nersc.gov/das/jupyterhub-base.gaffer:latest .
+branch=deploy-18-10
+docker build --no-cache -t registry.spin.nersc.gov/das/jupyterhub-base.$branch:latest .
