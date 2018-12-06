4d7ec8e29b814982cfa8e8938438ca7fe8d1c7c0
Rollin Thomas <rcthomas@lbl.gov>
2018-12-05 17:00:11 -0800

    Branch not so important

--- jupyter-off/build.sh
+++ jupyter-off/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-branch=$(git symbolic-ref --short HEAD)
-
 docker build    \
     "$@"        \
-    --tag registry.spin.nersc.gov/das/jupyter-off.$branch:latest .
+    --tag registry.spin.nersc.gov/das/jupyter-off:latest .
