d099b7f21b1bb09eb349e2ebe5bc64decc9dca5c
Rollin Thomas <rcthomas@lbl.gov>
2019-03-27 13:16:56 -0700

    Foolish consistency

--- jupyter-base/build.sh
+++ jupyter-base/build.sh
@@ -4,4 +4,4 @@ branch=$(git symbolic-ref --short HEAD)
 
 docker build    \
     --no-cache  \
-    --tag registry.spin.nersc.gov/das/jupyter-base.$branch:latest .
+    --tag registry.spin.nersc.gov/das/jupyter-base-$branch:latest .
