47dd2e7c5a45075885b743bc23236b4b7a2e8899
Rollin Thomas <rcthomas@lbl.gov>
2019-05-14 09:28:43 -0700

    Reframe query

--- jupyter-nersc/web-jupyterhub/iris.py
+++ jupyter-nersc/web-jupyterhub/iris.py
@@ -8,36 +8,31 @@ class Iris:
     def __init__(self, iris_url="https://iris.nersc.gov/graphql"):
         self.iris_url = iris_url
 
-    async def query_accounts(self, name):
+    async def query_user(self, name):
         query = dedent("""
-        query {{
-          systemInfo {{
-            users(name: "{}") {{
-              baseRepos {{
-                computeAllocation {{
-                  repoName
-                }}
-              }}
-            }}
-          }}
-          systemInfo {{
-            users(name: "{}") {{
-              userAllocations {{
-                computeAllocation {{
-                  repoName
-                }}
-              }}
-            }}
-          }}
-        }}""".format(name, name)).strip()
+		query {{
+		  systemInfo {{
+		    users(name: "{}") {{
+		      baseRepos {{
+		        computeAllocation {{
+		          repoName
+		        }}
+		      }}
+		      userAllocations {{
+		        computeAllocation {{
+		          repoName
+		        }}
+		        userAllocationQos {{
+		          qos {{
+		            qos
+		          }}
+		        }}
+		      }}
+		    }}
+		  }}
+		}}""".format(name)).strip()
         data = await self.query(query)
-        user = data["data"]["systemInfo"]["users"][0]
-        default_account = user["baseRepos"][0]["computeAllocation"]["repoName"]
-        accounts = [a["computeAllocation"]["repoName"] for a in user["userAllocations"]]
-        accounts.sort()
-        accounts.remove(default_account)
-        accounts.insert(0, default_account)
-        return accounts
+        return data["data"]["systemInfo"]["users"][0]
     
     async def query(self, query):
         client = httpclient.AsyncHTTPClient()
