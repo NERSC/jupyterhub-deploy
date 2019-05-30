89bd5f48274b3faa05f037772c419a9def385206
Rollin Thomas <rcthomas@lbl.gov>
2019-05-30 15:03:01 -0700

    Don't plan to push to registry

--- jupyter-compose/app-notebooks/build.sh
+++ jupyter-compose/app-notebooks/build.sh
@@ -5,4 +5,4 @@ branch=$(git symbolic-ref --short HEAD)
 docker build                    \
     --build-arg branch=$branch  \
     "$@"                        \
-    --tag registry.spin.nersc.gov/das/app-notebooks.jupyter-nersc-$branch:latest .
+    --tag app-notebooks:latest .
