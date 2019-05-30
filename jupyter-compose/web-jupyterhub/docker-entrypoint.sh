b44ab5036ff18e9bd1ed6672e04cdb6058811c8f
Rollin Thomas <rcthomas@lbl.gov>
2019-05-30 16:01:16 -0700

    Add a file

--- jupyter-compose/web-jupyterhub/docker-entrypoint.sh
+++ jupyter-compose/web-jupyterhub/docker-entrypoint.sh
@@ -3,6 +3,8 @@
 for u in $(ls /home/) ; do
   cp /config/newkey /tmp/$u.key
   chmod 600 /tmp/$u.key
+  cp /config/newkey-cert.pub /tmp/$u.key-cert.pub
+  chmod 600 /tmp/$u.key-cert.pub
 done
 
 exec "$@"
