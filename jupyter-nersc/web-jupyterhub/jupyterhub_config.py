1332cf5fdb5e271a25f8c96010db62d256d07e5f
Rollin Thomas <rcthomas@lbl.gov>
2019-07-25 12:20:30 -0700

    Pre-spawn hook off due to JupyterHub bug I think

--- jupyter-nersc/web-jupyterhub/jupyterhub_config.py
+++ jupyter-nersc/web-jupyterhub/jupyterhub_config.py
@@ -1121,12 +1121,6 @@ c.NERSCSpawner.spawners = {
 
 # Pre-spawn myquota check
 
-def space_error(home):
-    """Extra message pointing users to try spawning again from /hub/home.  """
-    home = url_path_join(home, 'home')
-    return ("There is insufficient space in your home directory; please clear up some files and then " +
-            "<a href='{home}'>navigate to the hub home</a> and start your server.".format(home=home))
-
 async def setup(spawner):
     username = spawner.user.name
     remote_host = "corijupyter.nersc.gov"
@@ -1134,18 +1128,19 @@ async def setup(spawner):
     certfile = keyfile + "-cert.pub"
     k = asyncssh.read_private_key(keyfile)
     c = asyncssh.read_certificate(certfile)
-    # print(username, remote_host, keyfile, certfile)
     async with asyncssh.connect(remote_host, username=username, 
             client_keys=[(k,c)], known_hosts=None) as conn:
         result = await conn.run("myquota -c $HOME")
         retcode = result.exit_status
     if retcode:
-        e = web.HTTPError(507,reason="Insufficient Storage")
-        em = space_error(spawner.hub.base_url)
-        e.my_message = em
+        from jinja2 import Markup
+        e = web.HTTPError(507, reason="Insufficient Storage")
+        e.jupyterhub_message = Markup("<br>Your home directory is over quota! " +
+                "Try moving or archiving and deleting some files from there, " +
+                "then come back and try again.<br>")
         raise e
 
-c.Spawner.pre_spawn_hook = setup
+#c.Spawner.pre_spawn_hook = setup
 
 ###
 
