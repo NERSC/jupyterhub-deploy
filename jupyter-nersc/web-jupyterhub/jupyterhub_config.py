ed12019dc7799126448e5356e55a7246e89fe615
Rollin Thomas <rcthomas@lbl.gov>
2019-08-06 11:49:49 -0700

    Not sitting with proxy anymore

--- jupyter-nersc/web-jupyterhub/jupyterhub_config.py
+++ jupyter-nersc/web-jupyterhub/jupyterhub_config.py
@@ -397,7 +397,7 @@ c.JupyterHub.services = [
     },
     {
         'name': 'announcement',
-        'url': 'http://127.0.0.1:8888',
+        'url': 'http://web-jupyterhub:8888',
         'command': ["python", "-m", "announcement"],
     },
     {
