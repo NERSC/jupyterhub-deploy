dcaf08d79b2934cc92481d81b2ee7cacc188aa7b
Rollin Thomas <rcthomas@lbl.gov>
2019-11-11 10:37:37 -0800

    Testing remote proxy

--- jupyter-compose/app-notebooks/jupyter_notebook_config.py
+++ jupyter-compose/app-notebooks/jupyter_notebook_config.py
@@ -1,5 +1,11 @@
 
-def mapper(key):
-    return "service" + key
+### def mapper(key):
+###     return "service" + key
+### 
+### c.ServerMapper.mapper = mapper
 
-c.ServerMapper.mapper = mapper
+def hook(handler, host):
+    handler.log.info("request to proxy to host " + host)
+    return host == "service1"
+
+c.ServerProxy.host_whitelist_hook = hook
