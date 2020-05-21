d98099e6ba6f4d3b0dad0147f2d756c5828d8fb5
Rollin Thomas <rcthomas@lbl.gov>
2020-05-21 12:51:08 -0700

    Work w/podman or docker, remove forced --no-cache

--- jupyter-base/build.sh
+++ jupyter-base/build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+imcmd=""
+for command in docker podman; do
+    if [ $(command -v $command) ]; then
+        imcmd=$command
+        break
+    fi
+done
+if [ -n "$imcmd" ]; then
+    echo "Using $imcmd"
+else
+    echo "No image command defined"
+    exit 1
+fi
+
 branch=$(git symbolic-ref --short HEAD)
 
-docker build    \
-    --no-cache  \
+$imcmd build    \
+    "$@"
     --tag registry.spin.nersc.gov/das/jupyter-base-$branch:latest .
