18ca71dbe6fd106939d7b3c5041777eba7b356fc
Rollin Thomas <rcthomas@lbl.gov>
2020-07-01 16:40:58 -0700

    Support podman or docker

--- jupyter-nersc/web-announcement/build.sh
+++ jupyter-nersc/web-announcement/build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+imcmd=""
+for command in docker podman; do
+    if [ $(command -v $command) ]; then
+        imcmd=$command
+        break
+    fi
+done
+if [ -n "$imcmd" ]; then
+    echo "Using $imcmd"
+else
+    echo "No image command defined"
+    exit 1
+fi
+
 branch=$(git symbolic-ref --short HEAD)
 
-docker build                    \
+$imcmd build                    \
     --build-arg branch=$branch  \
     "$@"                        \
     --tag registry.spin.nersc.gov/das/web-announcement.jupyter-nersc-$branch:latest .
